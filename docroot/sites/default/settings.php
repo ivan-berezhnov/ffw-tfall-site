53103d543f2ecf9c65a1707d47fa90aaabc37e1d
Michael Nowicki <michaelnowicki@appnovation.com>
2013-08-28 23:53:22 +0100

    fix settings.php

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -210,21 +210,7 @@
  *   );
  * @endcode
  */
-$databases = array (
-  'default' => 
-  array (
-    'default' => 
-    array (
-      'database' => 'teachforall',
-      'username' => 'root',
-      'password' => 'root',
-      'host' => 'localhost',
-      'port' => '',
-      'driver' => 'mysql',
-      'prefix' => '',
-    ),
-  ),
-);
+$databases = array();
 
 $databases['default']['default'] = array(
 	'driver' => 'mysql',
@@ -574,3 +560,12 @@ $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
  * Remove the leading hash signs to disable.
  */
 # $conf['allow_authorize_operations'] = FALSE;
+
+
+// On Acquia Cloud, this include file configures Drupal to use the correct
+// database in each site environment (Dev, Stage, or Prod). To use this 
+// settings.php for development on your local workstation, set $db_url
+// (Drupal 5 or 6) or $databases (Drupal 7) as described in comments above.
+if (file_exists('/var/www/site-php')) {
+  require('/var/www/site-php/pubteach4all/pubteach4all-settings.inc');
+}
