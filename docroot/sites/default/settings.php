1b2e5aedc955e036f3c3030de6dcafac2aae7fb4
AlexBrown-appnovation <alex@appnovation.com>
2013-08-28 00:34:30 -0700

    grabbing settings php from michaels version

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -210,7 +210,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'teachforall',
+      'username' => 'root',
+      'password' => 'root',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -242,7 +256,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = 'jz4w_mKLcNfGQ5ECrZ_1MbXtq1s2WYqjR5x0pFUSr_c';
 
 /**
  * Base URL (optional).
@@ -551,12 +565,3 @@ $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
  * Remove the leading hash signs to disable.
  */
 # $conf['allow_authorize_operations'] = FALSE;
-
-
-// On Acquia Cloud, this include file configures Drupal to use the correct
-// database in each site environment (Dev, Stage, or Prod). To use this 
-// settings.php for development on your local workstation, set $db_url
-// (Drupal 5 or 6) or $databases (Drupal 7) as described in comments above.
-if (file_exists('/var/www/site-php')) {
-  require('/var/www/site-php/pubteach4all/pubteach4all-settings.inc');
-}
