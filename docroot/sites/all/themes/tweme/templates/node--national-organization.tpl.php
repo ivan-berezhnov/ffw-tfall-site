1668ad0fd53a152eafc2971ea9dbbd57d7c85273
Alex Brown <alex@appnovation.com>
2013-09-06 10:36:04 -0700

    national organization style tweaks

--- docroot/sites/all/themes/tweme/templates/node--national-organization.tpl.php
+++ docroot/sites/all/themes/tweme/templates/node--national-organization.tpl.php
@@ -21,24 +21,38 @@
           <?php print render($content['field_ceo_profile']); ?>
         </div>
 
-        <div class="no__info-block">
+        <div class="no__info-block no__stats">
           <div class="slug"><?php print t('Program Snapshot'); ?></div>
 
-					<?php if (isset($content['field_program_launched'])): ?>
-						<b><?php print render($content['field_program_launched']); ?></b> program launched<br />
-					<?php endif; ?>
-					<?php if (isset($content['field_cohorts_placed'])): ?>
-						<b><?php print render($content['field_cohorts_placed']); ?></b> cohorts placed<br />
-					<?php endif; ?>
-					<?php if (isset($content['field_teachers_active'])): ?>
-						<b><?php print render($content['field_teachers_active']); ?></b> teachers active<br />
-					<?php endif; ?>
-					<?php if (isset($content['field_alumni'])): ?>
-						<b><?php print render($content['field_alumni']); ?></b> alumni<br />
-					<?php endif; ?>
-					<?php if (isset($content['field_schools'])): ?>
-						<b><?php print render($content['field_schools']); ?></b> schools in Lohore<br /> and Karachi<br />
-					<?php endif; ?>					
+          <div class="no__stat">
+  					<?php if (isset($content['field_program_launched'])): ?>
+  						<strong><?php print render($content['field_program_launched']); ?></strong> program launched<br />
+  					<?php endif; ?>
+          </div>
+
+          <div class="no__stat">
+  					<?php if (isset($content['field_cohorts_placed'])): ?>
+  						<strong><?php print render($content['field_cohorts_placed']); ?></strong> cohorts placed<br />
+  					<?php endif; ?>
+          </div>
+
+          <div class="no__stat">
+  					<?php if (isset($content['field_teachers_active'])): ?>
+  						<strong><?php print render($content['field_teachers_active']); ?></strong> teachers active<br />
+  					<?php endif; ?>
+          </div>
+
+          <div class="no__stat">
+  					<?php if (isset($content['field_alumni'])): ?>
+  						<strong><?php print render($content['field_alumni']); ?></strong> alumni<br />
+  					<?php endif; ?>
+          </div>
+
+          <div class="no__stat">
+  					<?php if (isset($content['field_schools'])): ?>
+  						<strong><?php print render($content['field_schools']); ?></strong> schools in Lohore<br /> and Karachi<br />
+  					<?php endif; ?>
+          </div>
 
         </div>
 
@@ -46,47 +60,53 @@
           <?php print render($content['field_no_image_map']); ?>
         </div>
 
-        <div class="no__info-block">
+        <div class="no__info-block no__stats">
           <div class="slug"><?php print t('Local Context'); ?></div>
 
-					<?php if (isset($content['field_population'])): ?>
-						<b><?php print render($content['field_population']); ?></b> population<br />
-					<?php endif; ?>
-					<?php if (isset($content['field_living_below_poverty_line'])): ?>
-						<b><?php print render($content['field_living_below_poverty_line']); ?>%</b> living below poverty line<br />
-					<?php endif; ?>
-					<?php if (isset($content['field_avg_total_years_of_edu'])): ?>
-						<b><?php print render($content['field_avg_total_years_of_edu']); ?></b> avr. total years of education<br />
-					<?php endif; ?>
+          <div class="no__stat">
+  					<?php if (isset($content['field_population'])): ?>
+  						<strong><?php print render($content['field_population']); ?></strong> population<br />
+  					<?php endif; ?>
+          </div>
+
+          <div class="no__stat">
+  					<?php if (isset($content['field_living_below_poverty_line'])): ?>
+  						<strong><?php print render($content['field_living_below_poverty_line']); ?>%</strong> living below poverty line<br />
+  					<?php endif; ?>
+          </div>
+
+          <div class="no__stat">
+  					<?php if (isset($content['field_avg_total_years_of_edu'])): ?>
+  						<strong><?php print render($content['field_avg_total_years_of_edu']); ?></strong> avr. total years of education<br />
+  					<?php endif; ?>
+          </div>
 
         </div>
 
       </div>
       <div class="span6">
         <?php print render($content['field_no_image']); ?>
-        <?php print render($content['field_blurb']); ?>
-				
-				
-				<div class="row-fluid">
+        <div class="no__blurb">
+          <?php print render($content['field_blurb']); ?>
+        </div>
+
+				<div class="row-fluid no__spotlights">
 					<div class="span6">
-						<?php print render($content['field_teacher_spotlight']); ?> 
+						<?php print render($content['field_teacher_spotlight']); ?>
 					</div>
 					<div class="span6">
 						<?php print render($content['field_alumni_spotlight']); ?>
 					</div>
 				</div>
-				
-				
+
       </div>
       <div class="span3">
-        Twitter Feed<br />
-				
+
 				<?php //temporary! generic facebook like-box... awaiting response TFAPW-56 ?>
 				<?php if (module_exists('widget_facebook')): ?>
 					<?php print render(widget_facebook_embed_homepage()); ?>
 				<?php endif; ?>
-				
-				
+
       </div>
     </div>
 
