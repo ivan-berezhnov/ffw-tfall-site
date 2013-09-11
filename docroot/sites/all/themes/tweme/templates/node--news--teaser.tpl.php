22290ee5e921e5be823b0e275c0ed85cf46b8c0d
Michael Nowicki <michaelnowicki@appnovation.com>
2013-09-11 17:36:52 +0100

    update news

---
+++ docroot/sites/all/themes/tweme/templates/node--news--teaser.tpl.php
@@ -0,0 +1,17 @@
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <div class="content"<?php print $content_attributes; ?>>
+
+		<div><?php print $title; ?></div>
+
+		<?php if (isset($content['field_news_byline'])): ?>
+			<div><?php print render($content['field_news_byline']); ?></div>
+		<?php endif; ?>
+
+		<a href="<?php print drupal_get_path_alias('node/' . $node->nid); ?>">read more...</a> <!-- link to news --> 
+		<div>
+			<?php print format_date($node->published_at, 'short'); ?> <!-- publish date --> 
+		</div>
+
+  </div>
+</div>
