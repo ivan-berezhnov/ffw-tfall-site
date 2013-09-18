95ef0564813af14e336ce2588a7fbd44056cebc4
Alex Brown <alex@appnovation.com>
2013-09-18 13:27:14 -0700

    body taken out and byline added in news flexslider

--- docroot/sites/all/themes/tweme/templates/views-view-fields--news--block-1.tpl.php
+++ docroot/sites/all/themes/tweme/templates/views-view-fields--news--block-1.tpl.php
@@ -29,7 +29,7 @@
   <div class="news__flexslider__info">
     <?php print $fields['field_category']->content; ?>
     <h2><?php print $fields['title']->content; ?></h2>
-    <div class="news__preview visible-desktop"><?php print $fields['body']->content; ?></div>
+    <div class="news__preview visible-desktop"><?php print $fields['field_news_byline']->content; ?></div>
     <div class="news__link hidden-phone">
       <?php print $fields['view_node']->content; ?>
     </div>
