18a431fb7ec4a274392f4500fe2d12df2468b283
Alex Brown <alex@appnovation.com>
2013-09-18 14:46:13 -0700

    national org city field added

--- docroot/sites/all/themes/tweme/templates/node--national-organization.tpl.php
+++ docroot/sites/all/themes/tweme/templates/node--national-organization.tpl.php
@@ -8,48 +8,55 @@
           <?php print render($content['field_no_logo']); ?>
         </div>
 
-        <div class="no__website-info no__info-block">
-          <div class="slug"><?php print t('Website'); ?></div>
-          <?php print render($content['field_links']); ?>
-        </div>
+        <?php if (isset($content['field_links'])): ?>
+          <div class="no__website-info no__info-block">
+            <div class="slug"><?php print t('Website'); ?></div>
+            <?php print render($content['field_links']); ?>
+          </div>
+        <?php endif; ?>
 
-        <div class="no__info-block no__ceo">
-          <div class="slug"><?php print t('CEO'); ?></div>
-          <?php print render($content['field_ceo_profile']); ?>
-        </div>
+        <?php if (isset($content['field_ceo_profile'])): ?>
+          <div class="no__info-block no__ceo">
+            <div class="slug"><?php print t('CEO'); ?></div>
+            <?php print render($content['field_ceo_profile']); ?>
+          </div>
+        <?php endif; ?>
 
         <div class="no__info-block no__stats">
           <div class="slug"><?php print t('Program Snapshot'); ?></div>
 
-          <div class="no__stat">
-  					<?php if (isset($content['field_program_launched'])): ?>
+          <?php if (isset($content['field_program_launched'])): ?>
+            <div class="no__stat">
   						<strong><?php print render($content['field_program_launched']); ?></strong> program launched<br />
-  					<?php endif; ?>
-          </div>
+  					</div>
+          <?php endif; ?>
 
-          <div class="no__stat">
-  					<?php if (isset($content['field_cohorts_placed'])): ?>
-  						<strong><?php print render($content['field_cohorts_placed']); ?></strong> cohorts placed<br />
-  					<?php endif; ?>
-          </div>
+          <?php if (isset($content['field_cohorts_placed'])): ?>
+            <div class="no__stat">
+    					<strong><?php print render($content['field_cohorts_placed']); ?></strong> cohorts placed<br />
+    				</div>
+          <?php endif; ?>
 
-          <div class="no__stat">
-  					<?php if (isset($content['field_teachers_active'])): ?>
-  						<strong><?php print render($content['field_teachers_active']); ?></strong> teachers active<br />
-  					<?php endif; ?>
-          </div>
+          <?php if (isset($content['field_teachers_active'])): ?>
+            <div class="no__stat">
+    					<strong><?php print render($content['field_teachers_active']); ?></strong> teachers active<br />
+            </div>
+          <?php endif; ?>
 
-          <div class="no__stat">
-  					<?php if (isset($content['field_alumni'])): ?>
-  						<strong><?php print render($content['field_alumni']); ?></strong> alumni<br />
-  					<?php endif; ?>
-          </div>
+          <?php if (isset($content['field_alumni'])): ?>
+            <div class="no__stat">
+    					<strong><?php print render($content['field_alumni']); ?></strong> alumni<br />
+            </div>
+          <?php endif; ?>
 
-          <div class="no__stat">
-  					<?php if (isset($content['field_schools'])): ?>
-  						<strong><?php print render($content['field_schools']); ?></strong> schools in Lohore<br /> and Karachi<br />
-  					<?php endif; ?>
-          </div>
+          <?php if (isset($content['field_schools'])): ?>
+            <div class="no__stat">
+  						<strong><?php print render($content['field_schools']); ?></strong> schools
+              <?php if (isset($content['field_city_1'])): ?>
+                in <?php print render($content['field_city_1']); ?>
+              <?php endif; ?>
+            </div>
+          <?php endif; ?>
 
         </div>
 
