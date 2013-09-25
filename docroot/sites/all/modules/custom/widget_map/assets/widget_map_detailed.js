5e2c9f90a5ba01e92aa4250af773ae0902d84861
Michael Nowicki <michaelnowicki@appnovation.com>
2013-09-25 16:24:36 +0100

    global problem landing page

---
+++ docroot/sites/all/modules/custom/widget_map/assets/widget_map_detailed.js
@@ -0,0 +1,44 @@
+(function($) {
+	Drupal.behaviors.widget_map = {
+		attach: function(context, settings) {
+
+
+			var map = L.mapbox.map('map', 'andrew-ho-tfall.pubteach4allmap', {
+				center: [30, 25],
+				zoom: 2,
+				minZoom: 2,
+				maxZoom: 5
+			});
+
+			map.doubleClickZoom.disable();
+			map.scrollWheelZoom.disable();
+
+			$.ajax({
+				url: '/widget_map_detailedlist.json',
+				success: function parseJson(data) {
+					map.markerLayer.setGeoJSON(data);
+				}
+			});
+
+
+			map.markerLayer.on('layeradd', function(e) {
+				var marker = e.layer,feature = marker.feature;
+
+				marker.setIcon(L.icon(feature.properties.icon));
+
+				var popupContent =
+								'<a class="popup" href="' + feature.properties.url + '">' +
+								'<h2>' + feature.properties.title + '</h2>' +
+								'<img src="' + feature.properties.logo + '">' +
+								'</a>' +
+								'<a class="btn btn-primary" href="' + feature.properties.url + '">Learn more</a>';
+
+				marker.bindPopup(popupContent, {
+					closeButton: false,
+					minWidth: 320
+				});
+			});
+
+		}
+	};
+}(jQuery));
