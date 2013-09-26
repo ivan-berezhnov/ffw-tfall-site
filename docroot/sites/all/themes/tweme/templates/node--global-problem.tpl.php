f05d0a2d4f3c9d2bb639dab4f8d19200729b785d
Alex Brown <alex@appnovation.com>
2013-09-26 11:47:02 -0700

    global problem initial template setup

--- docroot/sites/all/themes/tweme/templates/node--global-problem.tpl.php
+++ docroot/sites/all/themes/tweme/templates/node--global-problem.tpl.php
@@ -1,14 +1,38 @@
+<?php dsm($content['field_top_section']); ?>
 <div class="container">
   <h2 class="page-title"><?php print $title; ?></h2>
-  <?php print render($content['field_top_section']); ?>
-  <h2>
-  	<?php print render($content['field_globalproblem_subheader']); ?>
-  </h2>
+
+      <div class="row-fluid">
+        <div class="row-fluid">
+          <div class="span12">
+            <?php print render($content['field_top_section'][0]); ?>
+            <?php print render($content['field_top_section'][1]); ?>
+            <?php print render($content['field_top_section'][2]); ?>
+          </div>
+        </div>
+      </div>
+
+  <div class="row-fluid">
+    <div class="span12">
+      <h2><?php print render($content['field_globalproblem_subheader']); ?></h2>
+    </div>
+  </div>
 </div>
 
-<?php if (module_exists('widget_map')): ?>
-	<?php print render(widget_map_detailed_embed()); ?>
-<?php endif; ?>
+<div id="worldmap" class="container-wrapper">
+  <div class="container">
+    <!-- world map -->
+    <div class="row-fluid">
+      <div class="span12">
+        <?php if (module_exists('widget_map')): ?>
+          <?php $widget_map = widget_map_detailed_embed();
+            print render($widget_map);
+          ?>
+        <?php endif; ?>
+      </div>
+    </div>
+  </div>
+</div>
 
 <div class="container">
   <?php print render($content['body']); ?>
