c13e57c4031f814efc125186de7ec0a5f21c69ba
Alex Brown <alex@appnovation.com>
2013-09-26 10:37:08 -0700

    global problem container classes and title added

--- docroot/sites/all/themes/tweme/templates/node--global-problem.tpl.php
+++ docroot/sites/all/themes/tweme/templates/node--global-problem.tpl.php
@@ -1,12 +1,15 @@
-<?php print render($content['field_top_section']); ?>
-
-<h2>
-	<?php print render($content['field_globalproblem_subheader']); ?> 
-</h2>
+<div class="container">
+  <h2 class="page-title"><?php print $title; ?></h2>
+  <?php print render($content['field_top_section']); ?>
+  <h2>
+  	<?php print render($content['field_globalproblem_subheader']); ?>
+  </h2>
+</div>
 
 <?php if (module_exists('widget_map')): ?>
 	<?php print render(widget_map_detailed_embed()); ?>
 <?php endif; ?>
 
-
-<?php print render($content['body']); ?>
+<div class="container">
+  <?php print render($content['body']); ?>
+</div>
