231d40ba7a0f654562c71c2d49adb0ca45f1b947
Alex Brown <alex@appnovation.com>
2013-09-27 11:26:06 -0700

    news page template layout fix

--- docroot/sites/all/themes/tweme/templates/node-unformatted--news.tpl.php
+++ docroot/sites/all/themes/tweme/templates/node-unformatted--news.tpl.php
@@ -1,25 +1,23 @@
-<h2 class="page-title">NEWS</h2>
-<div class="row-fluid">
-  <div class="span12">
-    <div class="row-fluid">
-        <div class="row-fluid">
-          <div class="span9">
-            <?php print render(views_embed_view('news','block_1')); //flexslider ?>
-          </div>
+<div class="container">
+  <h2 class="page-title">NEWS</h2>
+  <div class="row-fluid">
+    <div class="span9">
+      <?php print render(views_embed_view('news','block_1')); //flexslider ?>
+      <div class="row-fluid">
+        <div class="span12">
+          <?php print render(views_embed_view('news','page')); ?>
         </div>
-        <div class="row-fluid">
-          <div class="span12">
-            <?php print render(views_embed_view('news','page')); ?>
-          </div>
-        </div>
-        <div class="row-fluid">
-          <div class="span12">
-            <a href="/news/latest"><div class="full-list">More News <span class="orange2">>></span></div></a>
-          </div>
-        </div>
-      <div class="span3">
-        <?php print render(views_embed_view('tfall_tweets','block')); //twitter block ?>
       </div>
+    </div>
+    <div class="span3">
+      <?php print render(views_embed_view('tfall_tweets','block')); //twitter block ?>
+      <?php $widget_facebook = widget_facebook_embed_homepage() //facebook widget; ?>
+      <?php print render($widget_facebook); ?>
+    </div>
+  </div>
+  <div class="row-fluid">
+    <div class="span12">
+      <a href="/news/latest"><div class="full-list">More News <span class="orange2">>></span></div></a>
     </div>
   </div>
 </div>
