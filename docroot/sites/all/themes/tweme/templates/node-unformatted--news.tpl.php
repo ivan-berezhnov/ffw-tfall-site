351f335310535fe15139d4bb4335f46db4283529
Alex Brown <alex@appnovation.com>
2013-09-27 10:30:18 -0700

    news landing page layout fix

--- docroot/sites/all/themes/tweme/templates/node-unformatted--news.tpl.php
+++ docroot/sites/all/themes/tweme/templates/node-unformatted--news.tpl.php
@@ -1,26 +1,40 @@
-<?php print render(views_embed_view('news','block_1')); //flexslider ?>
-
-
 <div class="container">
 
+  <h2 class="page-title">NEWS</h2>
+
   <div class="row-fluid">
-    <div class="span9">
-      <div class="row-fluid">
-        <div class="span12">
-          <?php print render(views_embed_view('news','page')); ?>
-        </div>
-      </div>
+    <div class="span12">
 
       <div class="row-fluid">
-        <div class="span12">
-          <a href="/news/latest"><div class="full-list">More News <span class="orange2">>></span></div></a>
+
+        <div class="span9">
+          <div class="row-fluid">
+            <div class="span12">
+              <?php print render(views_embed_view('news','block_1')); //flexslider ?>
+            </div>
+          </div>
+
+          <div class="row-fluid">
+            <div class="span12">
+              <?php print render(views_embed_view('news','page')); ?>
+            </div>
+          </div>
+
+          <div class="row-fluid">
+            <div class="span12">
+              <a href="/news/latest"><div class="full-list">More News <span class="orange2">>></span></div></a>
+            </div>
+          </div>
         </div>
+
+        <div class="span3">
+          <?php print render(views_embed_view('tfall_tweets','block')); //twitter block ?>
+        </div>
+
       </div>
+
     </div>
 
-    <div class="span3">
-			<?php print render(views_embed_view('tfall_tweets','block')); //tweeter block ?>
-		</div>
   </div>
 
 </div>
