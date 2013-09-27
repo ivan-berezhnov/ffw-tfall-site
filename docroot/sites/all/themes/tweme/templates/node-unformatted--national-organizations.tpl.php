d6e1c6b4564e654df5d3b31dc012bbdfaea15c9a
Michael Nowicki <michaelnowicki@appnovation.com>
2013-09-27 18:08:26 +0100

    node-unformatted--national-organizations.tpl preworks

---
+++ docroot/sites/all/themes/tweme/templates/node-unformatted--national-organizations.tpl.php
@@ -0,0 +1,18 @@
+<h2>National Organizations page v2</h2>
+
+TFAPW-122
+
+
+organizations are listed in three columns
+		all organizations are sorted alphabetically by country name, going from top to bottom of each column (not sideways across columns)
+		organizations are distributed as evenly as possible with the right most column being shorter than the others if needed
+				in some cases the left column would be longer than the two others, e.g. if there were only 28 organizations
+each organization is listed with
+		country name
+		organization name
+organization name is clickable and will take the user to the node page for that organization
+two content fields are available at the bottom of the page, side by side under a section title (see mockup http://www.teachforall.org/comps/tfall-national-orgs.html)
+page will have a button/link at the bottom with
+		title
+		internal target URL
+
