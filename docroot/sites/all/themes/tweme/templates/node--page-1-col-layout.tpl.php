3db8bd80893e718d724905e6a166ba934daf0504
Alex Brown <alex@appnovation.com>
2013-10-01 16:19:51 -0700

    added jump link to 1 page col

--- docroot/sites/all/themes/tweme/templates/node--page-1-col-layout.tpl.php
+++ docroot/sites/all/themes/tweme/templates/node--page-1-col-layout.tpl.php
@@ -82,30 +82,23 @@
 ?>
 
 <div class="container">
-<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+  <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
-  <?php //print $user_picture; ?>
+    <h2 class="page-title"><?php print $title; ?></h2>
 
-  <h2 class="page-title"><?php print $title; ?></h2>
-
-  <?php if ($display_submitted): ?>
-    <div class="submitted">
-      <?php print $submitted; ?>
+    <div class="row-fluid">
+      <div class="span12">
+        <?php print render($content['body']); ?>
+      </div>
     </div>
-  <?php endif; ?>
 
-  <div class="content"<?php print $content_attributes; ?>>
-    <?php
-      // We hide the comments and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
-      print render($content);
-    ?>
+    <div class="row-fluid">
+      <div class="span12">
+        <div class="jump-link">
+          <?php print render($content['field_link']); ?>
+        </div>
+      </div>
+    </div>
+
   </div>
-
-  <?php print render($content['links']); ?>
-
-  <?php //print render($content['comments']); ?>
-
-</div>
 </div>
