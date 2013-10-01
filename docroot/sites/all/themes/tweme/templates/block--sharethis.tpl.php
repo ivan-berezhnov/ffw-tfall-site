f1be8f566b70bef75f24b5ff4d277f6f8cb7c62e
Alex Brown <alex@appnovation.com>
2013-10-01 15:53:40 -0700

    sharethis widget for news and network learning and spotlights

---
+++ docroot/sites/all/themes/tweme/templates/block--sharethis.tpl.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Custom theme implementation to display a single carousel item.
+ */
+
+?>
+<div class="container">
+  <div id="<?php print $block_html_id ?>" class="<?php print $classes ?>"<?php print $attributes ?>>
+    <?php print $content ?>
+  </div>
+</div>
