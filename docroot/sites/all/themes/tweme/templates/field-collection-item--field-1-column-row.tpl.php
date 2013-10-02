beb44e04fae375d5ad04cb9a1cb49e06e32674b8
Alex Brown <alex@appnovation.com>
2013-10-02 16:04:30 -0700

    1 col layout template fix

---
+++ docroot/sites/all/themes/tweme/templates/field-collection-item--field-1-column-row.tpl.php
@@ -0,0 +1,17 @@
+<div class="row-fluid">
+  <div class="span12">
+    <?php print render($content['field_text_area']); ?>
+  </div>
+</div>
+
+<div class="row-fluid">
+  <div class="span12">
+    <?php print render($content['field_image']); ?>
+  </div>
+</div>
+
+<div class="row-fluid">
+  <div class="span12">
+    <?php print render($content['field_embedded_video']); ?>
+  </div>
+</div>
