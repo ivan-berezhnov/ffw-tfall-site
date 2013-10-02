f99217f61a03b49106f15ae9bb4d769e8dcecfa8
Alex Brown <alex@appnovation.com>
2013-10-02 15:34:40 -0700

    nodequeue marquee views fix

--- docroot/sites/all/themes/tweme/templates/views-view-fields--nodequeue-marquee--block.tpl.php
+++ docroot/sites/all/themes/tweme/templates/views-view-fields--nodequeue-marquee--block.tpl.php
@@ -1,50 +1,21 @@
 <?php
+  $vertical_image_node_number = 4;
+  $is_vertical_image_node_number = $vertical_image_node_number == $row->nodequeue_nodes_node_position;
+  $newsImage = $fields['field_news_image']->content;
+  $spotlightImage = $fields['field_spotlight_image']->content;
+  $verticalImage = $fields['field_vertical_image']->content;
 
-/**
- * @file
- * Default simple view template to all the fields as a row.
- *
- * - $view: The view in use.
- * - $fields: an array of $field objects. Each one contains:
- *   - $field->content: The output of the field.
- *   - $field->raw: The raw data for the field, if it exists. This is NOT output safe.
- *   - $field->class: The safe class id to use.
- *   - $field->handler: The Views field handler object controlling this field. Do not use
- *     var_export to dump this object, as it can't handle the recursion.
- *   - $field->inline: Whether or not the field should be inline.
- *   - $field->inline_html: either div or span based on the above flag.
- *   - $field->wrapper_prefix: A complete wrapper containing the inline_html to use.
- *   - $field->wrapper_suffix: The closing tag for the wrapper.
- *   - $field->separator: an optional separator that may appear before a field.
- *   - $field->label: The wrap label text to use.
- *   - $field->label_html: The full HTML of the label to use including
- *     configured element type.
- * - $row: The raw result object from the query, with all data it fetched.
- *
- * @ingroup views_templates
- */
-?>
-<!-- if there is a vertical image, use it, otherwise use main image -->
-<?php
-
-$vertical_image_node_number = 4;
-$is_vertical_image_node_number = $vertical_image_node_number == $row->nodequeue_nodes_node_position;
-$newsImage = $fields['field_news_image']->content;
-$spotlightImage = $fields['field_spotlight_image']->content;
-$verticalImage = $fields['field_vertical_image']->content;
-
-if ($is_vertical_image_node_number) {
-    if (isset($verticalImage)) {
-        print $verticalImage;
-    }
-} else {
-    if (isset($newsImage)) {
-        print $newsImage;
-    } elseif (isset($spotlightImage)) {
-        print $spotlightImage;
-    }
-}
-
+  if ($is_vertical_image_node_number) {
+      if (isset($verticalImage)) {
+          print $verticalImage;
+      }
+  } else {
+      if (isset($newsImage)) {
+          print $newsImage;
+      } elseif (isset($spotlightImage)) {
+          print $spotlightImage;
+      }
+  }
 ?>
 <div class="marquee__hover">
   <div class="marquee__info-wrapper">
@@ -72,16 +43,14 @@ if ($is_vertical_image_node_number) {
 
     <div class="marquee__link">
         <?php
-
-        $embedded_field = $row->_field_data['nid']['entity']->field_embedded_video;
-        $embedded_field_video_size= sizeof($embedded_field);
-        $is_embedded_video_field = ($embedded_field_video_size > 0);
-        print_r($is_embedded_video_field);;
-        if ($is_embedded_video_field) {
-            print $fields['view_node_2']->content;
-        } else {
-            print $fields['view_node_1']->content;
-        }
+          $embedded_field = $row->_field_data['nid']['entity']->field_embedded_video;
+          $embedded_field_video_size= sizeof($embedded_field);
+          $is_embedded_video_field = ($embedded_field_video_size > 0);
+          if ($is_embedded_video_field) {
+              print $fields['view_node_2']->content;
+          } else {
+              print $fields['view_node_1']->content;
+          }
         ?>
     </div>
   </div>
