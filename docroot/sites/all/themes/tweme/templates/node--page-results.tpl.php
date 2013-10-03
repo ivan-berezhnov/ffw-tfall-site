33be790fcb860de48777ffef5db8c088171e19a5
Michael Nowicki <michaelnowicki@appnovation.com>
2013-10-03 15:52:23 +0100

    page - results

---
+++ docroot/sites/all/themes/tweme/templates/node--page-results.tpl.php
@@ -0,0 +1,44 @@
+<div class="container">
+  <h2 class="page-title"><?php print $title; ?></h2>
+
+  <div class="row-fluid our-model-banner">
+    <div class="span8 offset2">
+      <div class="field__text-area">
+				<?php print render($content['field_nov2_intro']); ?>
+      </div>
+    </div>
+  </div>
+
+	<div class="row-fluid our-model-section">
+    <div class="span12">
+			<div class="spotlights row-fluid">
+        <div class="span6"><?php print render($content['field_impact_through'][0]); ?></div>
+        <div class="span6"><?php print render($content['field_impact_through'][1]); ?></div>
+      </div>
+    </div>
+  </div>
+
+  <div class="row-fluid our-model-banner">
+    <div class="span8 offset2">
+      <div class="field__text-area">
+				<?php print render($content['field_summary']); ?>
+      </div>
+    </div>
+  </div>	
+
+	<div class="row-fluid our-model-banner">
+    <div class="span8 offset2">
+      <div class="field__text-area">
+				<?php print render($content['field_ourmodel_infographic']); ?>
+      </div>
+    </div>
+  </div>	
+
+  <div class="row-fluid">
+    <div class="span12">
+      <div class="jump-link">
+				<?php print render($content['field_links']); ?>
+      </div>
+    </div>
+  </div>
+</div>
--- hooks/common/post-code-deploy/common.sh
+++ hooks/common/post-code-deploy/common.sh
@@ -9,6 +9,7 @@ drush_alias=$site'.'$target_env
 
 drush @$drush_alias en our_model -y
 drush @$drush_alias en global_organization -y
+drush @$drush_alias en results -y
 
 echo "Update database:"
 drush @$drush_alias updatedb -y
