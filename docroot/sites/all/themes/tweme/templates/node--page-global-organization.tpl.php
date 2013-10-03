dbf5662331f26115c8faf1178890fe7b66103730
Alex Brown <alex@appnovation.com>
2013-10-03 11:27:57 -0700

    removed twitter api field and jump link tweaks to global org feature and global org theming

--- docroot/sites/all/themes/tweme/templates/node--page-global-organization.tpl.php
+++ docroot/sites/all/themes/tweme/templates/node--page-global-organization.tpl.php
@@ -2,7 +2,7 @@
   <h2 class="page-title"><?php print $title; ?></h2>
 
   <div class="row-fluid our-model-banner">
-    <div class="span8 offset2">
+    <div class="span10 offset1">
       <div class="field__text-area">
         <?php print render($content['field_nov2_intro']); ?>
       </div>
@@ -11,9 +11,11 @@
 
 	<div class="row-fluid our-model-section">
     <div class="span12">
-      <h3>TEACH FOR ALL ACCELERATES IMPACT THROUGH:</h3>
+      <h3 class="slug-large">TEACH FOR ALL ACCELERATES IMPACT THROUGH:</h3>
 			<div class="spotlights row-fluid">
-        <div class="span6"><?php print render($content['field_impact_through'][0]); ?></div>
+        <div class="span6">
+          <?php print render($content['field_impact_through'][0]); ?>
+        </div>
         <div class="span6"><?php print render($content['field_impact_through'][1]); ?></div>
       </div>
 			<div class="spotlights row-fluid">
