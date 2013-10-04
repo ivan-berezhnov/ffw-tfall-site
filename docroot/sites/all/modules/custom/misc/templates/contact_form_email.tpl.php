354b122187de481e4090a38af81a5b7fd8482726
Michael Nowicki <michaelnowicki@appnovation.com>
2013-10-04 17:15:22 +0100

    contact us form

---
+++ docroot/sites/all/modules/custom/misc/templates/contact_form_email.tpl.php
@@ -0,0 +1,7 @@
+<h2>This is email from website:</h2>
+<hr>
+First name: <?php print $content['firstname']; ?> <br />
+email addres: <?php print $content['email']; ?> <br />
+Country: <?php print $content['country']; ?> <br />
+Summary: <?php print $content['summary']; ?> <br />
+Statement: <?php print $content['statement']; ?> <br />
---
+++ docroot/sites/all/themes/tweme/templates/node-unformatted--contact-thank-you.tpl.php
@@ -0,0 +1,8 @@
+<div class="container">
+  <h2 class="page-title">Thank you for your email</h2>
+  <div class="row-fluid">
+    <div class="span8 offset2">Someone from our team will contact you shortly
+    </div>
+  </div>
+
+</div>
