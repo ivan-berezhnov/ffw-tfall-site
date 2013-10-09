e061c4d8592bed2ab45aa8f349256e24abc5a8fb
Alex Brown <alex@appnovation.com>
2013-10-09 11:45:45 -0700

    new media block field collection template and update font declarations

---
+++ docroot/sites/all/themes/tweme/templates/field-collection-item--field-media-block.tpl.php
@@ -0,0 +1,7 @@
+<div class="row-fluid">
+  <div class="span12">
+    <?php print render($content['field_image']); ?>
+    <?php print render($content['field_headline']); ?>
+    <?php print render($content['field_text_area']); ?>
+  </div>
+</div>
