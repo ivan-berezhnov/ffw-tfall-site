f7255ad24d1e3e6892a60d41e48566fb7141571e
David Andrews <dave@madeofpixels.ca>
2013-10-10 15:10:07 -0700

    Title and zoom control fade for National Statistics map

--- docroot/sites/all/modules/custom/widget_map/assets/widget_map_detailed.js
+++ docroot/sites/all/modules/custom/widget_map/assets/widget_map_detailed.js
@@ -2,13 +2,14 @@
 	Drupal.behaviors.widget_map = {
 		attach: function(context, settings) {
 
-
 			var map = L.mapbox.map('map', Drupal.settings.widget_map.key, {
 				center: [30, 25],
 				zoom: 2,
 				minZoom: 2,
 				maxZoom: 5
-			});
+			}),
+			$mapTitle = $('.worldmap__title'),
+			$zoomControls = $('.leaflet-control-zoom');
 
 			map.doubleClickZoom.disable();
 			map.scrollWheelZoom.disable();
@@ -17,9 +18,9 @@
 				url: '/widget_map_detailedlist.json',
 				success: function parseJson(data) {
 					map.markerLayer.setGeoJSON(data);
-				}								
+				}
 			});
-	
+
 			map.markerLayer.on('layeradd', function(e) {
 				var marker = e.layer,feature = marker.feature;
 
@@ -36,14 +37,16 @@
 			});
 
 			map.markerLayer.on('mouseover',function(e) {
+				$mapTitle.hide()
+				$zoomControls.hide();
 				e.layer.openPopup();
 			});
 
 			map.markerLayer.on('mouseout',function(e) {
+				$mapTitle.show();
+				$zoomControls.show();
 				e.layer.closePopup();
 			});
-
-
 		}
 	};
 }(jQuery));
