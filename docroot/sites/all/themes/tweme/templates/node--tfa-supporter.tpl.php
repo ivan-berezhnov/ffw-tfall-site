929fd9fd34589d6f0fda00bf8e81affa97c90c28
Harp Nagra <harp@appnovation.com>
2013-10-15 13:54:30 -0700

    Removed text field for URL and replaced with Link type field. Edited template to reflect changes.

--- docroot/sites/all/themes/tweme/templates/node--tfa-supporter.tpl.php
+++ docroot/sites/all/themes/tweme/templates/node--tfa-supporter.tpl.php
@@ -1,4 +1,4 @@
 <?php print $title; ?><br />
 <?php print render($content['field_tfa_supporter_logo']); ?><br/> 
-<?php print render($content['field_tfa_supporter_url']); ?> <br />
+<?php print render($content['field_tfa_supporter_link']); ?> <br />
 <?php print render($content['field_tfa_supporter_description']); ?> <br />
