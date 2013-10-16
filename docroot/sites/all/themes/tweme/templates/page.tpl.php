4cbe7afebd2d3c067b72579a80c7526b24cb9cbe
Harp Nagra <harp@appnovation.com>
2013-10-16 15:36:39 -0700

    changed donate link to get-involved/donate

--- docroot/sites/all/themes/tweme/templates/page.tpl.php
+++ docroot/sites/all/themes/tweme/templates/page.tpl.php
@@ -11,7 +11,7 @@
     </div>
     <div class="pull-right">
       <div class="donate pull-left">
-        <a href="/donate"><?php print t('DONATE'); ?></a>
+        <a href="/get-involved/donate"><?php print t('DONATE'); ?></a>
       </div>
       <a href="https://twitter.com/TeachForAll" target="_blank" class="social-media ico-twitter"></a>
       <a href="https://facebook.com/teachforall" target="_blank" class="social-media ico-facebook"></a>
@@ -117,7 +117,7 @@
       </div>
       <div class="span3">
         <div class="donate pull-left">
-          <a href="/donate"><?php print t('DONATE'); ?></a>
+          <a href="/get-involved/donate"><?php print t('DONATE'); ?></a>
         </div>
         <a href="https://twitter.com/TeachForAll" target="_blank" class="social-media ico-twitter"></a>
         <a href="https://facebook.com/teachforall" target="_blank" class="social-media ico-facebook"></a>
