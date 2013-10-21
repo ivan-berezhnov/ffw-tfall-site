b96d6571f0c341b05e945227228654afe1748d1a
Alex Brown <alex@appnovation.com>
2013-10-21 10:49:30 -0700

    supporter text fix

--- docroot/sites/all/modules/custom/widget_supporter/templates/widget-supporter.tpl.php
+++ docroot/sites/all/modules/custom/widget_supporter/templates/widget-supporter.tpl.php
@@ -1,12 +1,3 @@
-<?php
-/*
- * $logos - html list, view generated
- */
-
-?>
-
-<div>
-	<span class="title">SUPPORTERS</span>
-	<p>Teach For All is grateful<br />to our global partners for their generous support</p>
-	<?php print $logos; ?>
-</div>
+<span class="title">SUPPORTERS</span>
+<p>Teach For All is grateful<br />to our Global Champions for their generous support</p>
+<?php print $logos; ?>
