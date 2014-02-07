e7f6f61fd7c3d0db10a880c3ede6d211b4b09d12
Harp Nagra <harp@appnovation.com>
2014-02-07 01:32:39 -0800

    reformatted detailed

--- docroot/sites/all/modules/custom/widget_map/assets/widget_map_detailed.js
+++ docroot/sites/all/modules/custom/widget_map/assets/widget_map_detailed.js
@@ -1,93 +1,93 @@
-(function($) {
-	Drupal.behaviors.widget_map = {
-		attach: function(context, settings) {
+(function ($) {
+    Drupal.behaviors.widget_map = {
+        attach: function (context, settings) {
 
-			var map = L.mapbox.map('map', Drupal.settings.widget_map.key, {
-				center: [30, 25],
-				zoom: 2,
-				minZoom: 2,
-				maxZoom: 5
-			}),
-			$mapTitle = $('.worldmap__title'),
-			$zoomControls = $('.leaflet-control-zoom'),
-			hasTouch = $('html').hasClass('touch');
+            var map = L.mapbox.map('map', Drupal.settings.widget_map.key, {
+                    center: [30, 25],
+                    zoom: 2,
+                    minZoom: 2,
+                    maxZoom: 5
+                }),
+                $mapTitle = $('.worldmap__title'),
+                $zoomControls = $('.leaflet-control-zoom'),
+                hasTouch = $('html').hasClass('touch');
 
-			map.doubleClickZoom.disable();
-			map.scrollWheelZoom.disable();
+            map.doubleClickZoom.disable();
+            map.scrollWheelZoom.disable();
 
-			$.ajax({
-				url: '/widget_map_detailedlist.json',
-				success: function parseJson(data) {
-					map.markerLayer.setGeoJSON(data);
-				}
-			});
+            $.ajax({
+                url: '/widget_map_detailedlist.json',
+                success: function parseJson(data) {
+                    map.markerLayer.setGeoJSON(data);
+                }
+            });
 
-			toggleMapTitle = function(element) {
-				var $mapPopup = $('.leaflet-popup-pane');
+            toggleMapTitle = function (element) {
+                var $mapPopup = $('.leaflet-popup-pane');
 
-				switch(element) {
-					case 'marker':
-						// Note: added 200ms delay for iOS7, as it was reaching the conditional before the the popup rendered
-						setTimeout(function() {
-							if ($mapPopup.children().length) {
-								$mapTitle.hide();
-								$zoomControls.hide();
-							} else {
-								$mapTitle.show();
-								$zoomControls.show();
-							}
-						}, (hasTouch ? 200 : 0) );
+                switch (element) {
+                    case 'marker':
+// Note: added 200ms delay for iOS7, as it was reaching the conditional before the the popup rendered
+                        setTimeout(function () {
+                            if ($mapPopup.children().length) {
+                                $mapTitle.hide();
+                                $zoomControls.hide();
+                            } else {
+                                $mapTitle.show();
+                                $zoomControls.show();
+                            }
+                        }, (hasTouch ? 200 : 0));
 
-						break;
-					case 'map':
-						if ($mapPopup.children().length) {
-						  $mapTitle.hide();
-						  $zoomControls.hide();
-						} else {
-						  $mapTitle.show();
-						  $zoomControls.show();
-						}
+                        break;
+                    case 'map':
+                        if ($mapPopup.children().length) {
+                            $mapTitle.hide();
+                            $zoomControls.hide();
+                        } else {
+                            $mapTitle.show();
+                            $zoomControls.show();
+                        }
 
-						break;
-				}
-			}
+                        break;
+                }
+            }
 
-			map.markerLayer.on('layeradd', function(e) {
-				var marker = e.layer,feature = marker.feature;
+            map.markerLayer.on('layeradd', function (e) {
+                var marker = e.layer, feature = marker.feature;
 
-				marker.setIcon(L.icon(feature.properties.icon));
+                marker.setIcon(L.icon(feature.properties.icon));
 
-				var popupContent =
-								'<div class="slug">' + feature.properties.title + '</div>' +
-								'<div class="widget-map__blurb">' + feature.properties.blurb + '</div>';
+                var popupContent =
+                    '<div class="slug">' + feature.properties.title + '</div>' +
+                        '<div class="widget-map__blurb">' + feature.properties.blurb + '</div>';
 
-				marker.bindPopup(popupContent, {
-					closeButton: false,
-					minWidth: 320
-				});
-			});
+                marker.bindPopup(popupContent, {
+                    closeButton: false,
+                    minWidth: 320
+                });
+            });
 
-			map.markerLayer.on('mouseover',function(e) {
-				$mapTitle.hide()
-				$zoomControls.hide();
-				e.layer.openPopup();
-			});
+            map.markerLayer.on('mouseover', function (e) {
+                $mapTitle.hide()
+                $zoomControls.hide();
+                e.layer.openPopup();
+            });
 
-			map.markerLayer.on('mouseout',function(e) {
-				$mapTitle.show();
-				$zoomControls.show();
-				e.layer.closePopup();
-			});
+            map.markerLayer.on('mouseout', function (e) {
+                $mapTitle.show();
+                $zoomControls.show();
+                e.layer.closePopup();
+            });
 
-			if (hasTouch) {
-				map.markerLayer.on('click', function(e) {
-					toggleMapTitle('marker');
-				});
+            if (hasTouch) {
+                map.markerLayer.on('click', function (e) {
+                    toggleMapTitle('marker');
+                });
 
-				map.on('click', function() {
-					toggleMapTitle('map');
-				});
-			}
-		}
-	};
+                map.on('click', function () {
+                    toggleMapTitle('map');
+                });
+            }
+        }
+    };
 }(jQuery));
