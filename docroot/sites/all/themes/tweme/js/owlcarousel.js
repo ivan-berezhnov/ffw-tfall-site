61b273b1609c1b62070af1f90c07d732745f8263
DanAppnovation <dan@appnovation.com>
2014-03-18 15:48:07 -0700

    themed slider

--- docroot/sites/all/themes/tweme/js/owlcarousel.js
+++ docroot/sites/all/themes/tweme/js/owlcarousel.js
@@ -1,28 +1,22 @@
+(function ($) {
+  /* @functionality: custom slider */
+  Drupal.behaviors.owlSlider = {
+    attach: function (context, settings) {
 
+      $('.view-id-tfall_marquee', context).once( 'owlSlider', function () {
+        var slides = jQuery('.view-id-tfall_marquee .view-content',context);
+        slides.owlCarousel({
 
+        	items: 4,
+        	navigation: true,
+        	navigationText: ["prev","next"],
+        	scrollPerPage: true,
+        	pagination: false,
+        	autoHeight: false,
 
-    	jQuery(document).ready(function() {
+        });
 
-
-      jQuery('.view-id-tfall_marquee').once( 'customSlider', function () {
-        var slides = jQuery('.view-id-tfall_marquee .view-content');
-        slides.owlCarousel();
-		//get carousel instance data and store it in variable owl
-		var owl = slides.data('owlCarousel');
-
-		//Public methods
-		owl.next()   // Go to next slide
-		owl.prev()   // Go to previous slide
-		owl.goTo(x)  // Go to x slide
-		owl.jumpTo(x)  // Go to x slide without slide animation
-		 
-		//Auto Play
-		owl.play() // Autoplay
-		owl.stop() // Autoplay Stop
-		 
       });
-
-
-  		});
-
-
+    }
+  }
+})(jQuery);
