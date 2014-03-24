d9630a2617f502e3bc435c5532ab768d6409caa7
DanAppnovation <dan@appnovation.com>
2014-03-24 15:39:53 -0700

    added owl carousel

---
+++ docroot/sites/all/themes/tweme/js/owlcarousel.js
@@ -0,0 +1,28 @@
+(function ($) {
+  /* @functionality: custom slider */
+  Drupal.behaviors.owlSlider = {
+    attach: function (context, settings) {
+
+      $('.view-id-tfall_marquee', context).once( 'owlSlider', function () {
+        var slides = jQuery('.view-id-tfall_marquee .view-content',context);
+        slides.owlCarousel({
+
+        	items: 4,
+        	navigation: true,
+        	navigationText: ["prev","next"],
+        	scrollPerPage: true,
+        	pagination: false,
+        	autoHeight: false,
+        	mouseDrag: true,
+        	touchDrag: true,
+
+        	itemsTablet: [1024,2],
+        	itemsTabletSmall: [768,1],
+
+
+        });
+
+      });
+    }
+  }
+})(jQuery);
