13da661c30d3f192bd6c9c4d2d152b7aa7eb81a6
DanAppnovation <dan@appnovation.com>
2014-04-08 12:30:46 -0700

    more functionality fixes

--- docroot/sites/all/themes/tweme/js/owlcarousel.js
+++ docroot/sites/all/themes/tweme/js/owlcarousel.js
@@ -4,9 +4,9 @@
     attach: function (context, settings) {
 
       $('.view-id-tfall_marquee', context).once( 'owlSlider', function () {
-        var slides = jQuery('.view-id-tfall_marquee .view-content',context);
-        slides.owlCarousel({
 
+        var slides = $('.view-id-tfall_marquee .view-content',context);
+        slides.owlCarousel({
         	items: 4,
         	navigation: true,
         	navigationText : false,
@@ -16,11 +16,29 @@
         	mouseDrag: true,
         	touchDrag: true,
           autoPlay: true,
+          lazyLoad: true,
           itemsScaleUp: false,
+          rewindNav: false,
         	itemsTablet: [1024,2],
         	itemsTabletSmall: [420,1],
+          afterInit: function(){
+            if(this.currentItem === 0){
+              $('.owl-prev').hide();
+            }
+          },
+          afterMove: function(){
+            if(this.currentItem + 4 === this.itemsAmount){
+              $('.owl-next').hide();
+            }else{
+              $('.owl-next').show();
+            }
+            if(this.currentItem >= 1){
+              $('.owl-prev').show();
+            }else{
+              $('.owl-prev').hide();
+            }
+          },
         });
-
       });
     }
   }
