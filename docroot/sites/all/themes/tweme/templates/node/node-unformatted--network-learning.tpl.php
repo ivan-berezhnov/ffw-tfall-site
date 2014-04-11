03be30e46849044b8e1324ecd887df88608ddb2f
Harp Nagra <harp@appnovation.com>
2014-04-11 16:24:13 -0700

    Added class shadowboxvideo to vimeo and youttube videos.
    minor fixes

--- docroot/sites/all/themes/tweme/templates/node/node-unformatted--network-learning.tpl.php
+++ docroot/sites/all/themes/tweme/templates/node/node-unformatted--network-learning.tpl.php
@@ -2,15 +2,21 @@
   <h2 class="page-title"><?php print t('NETWORK LEARNING'); ?></h2>
   <div class="row-fluid">
     <div class="span9">
-      <?php print render(views_embed_view('network_learning','block_1')); //flexslider ?>
+      <?php
+      $network_learning_block1 = views_embed_view('network_learning','block_1');
+      print render($network_learning_block1); //flexslider ?>
       <div class="row-fluid">
         <div class="span12">
-          <?php print render(views_embed_view('network_learning','page')); ?>
+          <?php
+          $network_learning_page = views_embed_view('network_learning','page');
+          print render($network_learning_page); ?>
         </div>
       </div>
     </div>
     <div class="span3">
-      <?php print render(views_embed_view('tfall_tweets','block')); //twitter block ?>
+      <?php
+      $tfall_tweets_block = views_embed_view('tfall_tweets','block');
+      print render($tfall_tweets_block); //twitter block ?>
       <?php $widget_facebook = widget_facebook_embed_homepage() //facebook widget; ?>
       <?php print render($widget_facebook); ?>
     </div>
