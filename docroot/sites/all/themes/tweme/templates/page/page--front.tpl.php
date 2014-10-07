0099f7e0efd67240605a3716207c9de681f75ab4
Diana Dupuis <diana.dupuis@amazee.com>
2014-10-07 18:01:57 -0500

    map and tstring changes to make them translatable

--- docroot/sites/all/themes/tweme/templates/page/page--front.tpl.php
+++ docroot/sites/all/themes/tweme/templates/page/page--front.tpl.php
@@ -166,7 +166,7 @@
         <a href="https://vimeo.com/teachforall" target="_blank" class="social-media icon-vimeo"></a>
 		<a href="/news/feed" target="_blank" class="social-media icon-feed"></a>
         <div class="footer__search">
-          <p>Search the Site</p>
+          <p><?php print t('Search the Site'); ?></p>
           <?php print $navbar_search ?>
         </div>
         <div class="footer__contact-us"><a href="/contact-us">Contact Us</a></div>
