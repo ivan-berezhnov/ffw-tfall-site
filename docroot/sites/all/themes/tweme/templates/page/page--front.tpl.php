3f4b6b936a3a843d53b40b074ab9f4a12824646b
Diana Dupuis <diana.dupuis@amazee.com>
2014-10-10 16:58:12 -0500

    fix Friday malformation and hide language switcher

--- docroot/sites/all/themes/tweme/templates/page/page--front.tpl.php
+++ docroot/sites/all/themes/tweme/templates/page/page--front.tpl.php
@@ -33,12 +33,12 @@
       <a href="https://www.linkedin.com/company/teach-for-all" target="_blank" class="social-media icon-linkedin"></a>
       <a href="https://vimeo.com/teachforall" target="_blank" class="social-media icon-vimeo"></a>
 	  <a href="/news/feed" target="_blank" class="social-media icon-feed"></a>
-	  <?php $block = module_invoke('locale', 'block_view', 'language'); print $block['content']; ?>
+	  <?php //$block = module_invoke('locale', 'block_view', 'language'); print $block['content']; ?>
     </div>
   </div>
   <div class="row-fluid site-name">
     <div class="span12">
-      <a href="<?php print $front_page; ?>>"<img src="/sites/all/themes/tweme/images/prod/tfa-logo@x2.png" srcset="/sites/all/themes/tweme/images/prod/tfa-logo.png 1x, /sites/all/themes/tweme/images/prod/tfa-logo.png 1.5x, /sites/all/themes/tweme/images/prod/tfa-logo@x2.png 2x"></a>
+      <a href="<?php print $front_page; ?>"><img src="/sites/all/themes/tweme/images/prod/tfa-logo@x2.png" srcset="/sites/all/themes/tweme/images/prod/tfa-logo.png 1x, /sites/all/themes/tweme/images/prod/tfa-logo.png 1.5x, /sites/all/themes/tweme/images/prod/tfa-logo@x2.png 2x"></a><!<?php //print $front_page; ?>!>
     </div>
   </div>
 </div>
