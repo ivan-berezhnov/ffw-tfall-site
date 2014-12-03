42fffed5a9435f4bbcc6d74b26a30e15fa6b3bb0
Kathryn McClintock <kathryn@amazeelabs.com>
2014-12-03 15:24:04 -0600

    TFA-50: Fedra Serif from local font to Typotheque webfont

--- docroot/sites/all/themes/tweme/templates/system/html.tpl.php
+++ docroot/sites/all/themes/tweme/templates/system/html.tpl.php
@@ -20,7 +20,7 @@
     <!--[if lte IE 8]>
     <link rel="stylesheet" type="text/css" href="/sites/all/themes/tweme/stylesheets/ie8.css">
     <![endif]-->
-    <!--<link rel="stylesheet" href="http://fonts.typotheque.com/WF-021707-002785.css" type="text/css" />-->
+    <link rel="stylesheet" href="http://fonts.typotheque.com/WF-021707-002785.css" type="text/css" />
     <?php print $scripts ?>
     <?php print $head_bottom ?>
   </head>
