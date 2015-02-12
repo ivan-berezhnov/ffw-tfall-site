f8f3bffab92f5dafeaa10690e844ff92018b1066
yigenana <kathrynjonas@gmail.com>
2015-02-12 14:54:54 -0500

    feature and module updates

--- docroot/sites/all/themes/tweme/templates/system/html.tpl.php
+++ docroot/sites/all/themes/tweme/templates/system/html.tpl.php
@@ -26,6 +26,15 @@
     <?php print $head_bottom ?>
   </head>
   <body class="<?php print $classes ?>"<?php print $attributes ?>>
+    <!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-5SF2J5"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5SF2J5');</script>
+<!-- End Google Tag Manager -->
     <div id="fb-root"></div>
     <?php print $page_top ?>
     <?php print $page ?>
