93cda982bc0b0b63cc4f6762623b3f974a78a6ea
Kathryn McClintock <kathryn@amazeelabs.com>
2015-03-05 16:58:30 -0600

    TFA-65: Completed desktop styles for the Forum landing page

--- docroot/sites/all/themes/tweme/templates/node/node--tfa-opinion--alt-teaser.tpl.php
+++ docroot/sites/all/themes/tweme/templates/node/node--tfa-opinion--alt-teaser.tpl.php
@@ -1,18 +1,20 @@
 <?php
   hide($content);
 ?>
-  
+
 <span id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>">
 <span class="opinion-author-image">
 <?php print theme('image_style', array('style_name' => $content['field_tfa_opinion_image'][0]['#image_style'] , 'path' => $content['field_tfa_opinion_image'][0]['#item']['uri'])); ?>
 </span>
-<span class="opinion-author-info">
-<?php print render($content['field_tfa_opinion_author']); ?>
-</span>
-<span>
-<?php print render($content['field_tfa_opinion_job_title']); ?>
-</span>
-<span>
-<?php print render($content['field_tfa_opinion_author_bio']); ?>
+<div class="opinion-author-info">
+  <span class="opinion-author-name">
+    <?php print render($content['field_tfa_opinion_author']); ?>
+  </span>
+  <span class="opinion-author-title">
+    <?php print render($content['field_tfa_opinion_job_title']); ?>
+  </span>
+</div>
+<span class="opinion-author-bio">
+  <?php print render($content['field_tfa_opinion_author_bio']); ?>
 </span>
 </span>
