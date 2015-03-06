b79dbd861df902ef4b7e468c7c955bfec4070e8c
Jason Savino <jason@amazeelabs.com>
2015-03-06 14:21:53 -0800

    TFA-65 Create Forum Landing Page

--- docroot/sites/all/themes/tweme/templates/node/node--tfa-opinion--alt-teaser.tpl.php
+++ docroot/sites/all/themes/tweme/templates/node/node--tfa-opinion--alt-teaser.tpl.php
@@ -6,15 +6,15 @@
 <span class="opinion-author-image">
 <?php print theme('image_style', array('style_name' => $content['field_tfa_opinion_image'][0]['#image_style'] , 'path' => $content['field_tfa_opinion_image'][0]['#item']['uri'])); ?>
 </span>
-<div class="opinion-author-info">
+<span class="opinion-author-info">
   <span class="opinion-author-name">
     <?php print render($content['field_tfa_opinion_author']); ?>
   </span>
   <span class="opinion-author-title">
     <?php print render($content['field_tfa_opinion_job_title']); ?>
   </span>
-</div>
-<span class="opinion-author-bio">
-  <?php print $title; ?>
+  <span class="opinion-author-bio">
+    <?php print $title; ?>
+  </span>
 </span>
 </span>
