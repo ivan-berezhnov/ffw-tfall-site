661a4d5d951e37e88d78e7555b01be738d113c8c
Kathy Chavez <kappaluppa2@gmail.com>
2016-01-28 19:27:08 -0800

    replace fieldable-panels-pane.tpl.php

--- docroot/sites/all/themes/tweme/templates/panopoly/fieldable-panels-pane.tpl.php
+++ docroot/sites/all/themes/tweme/templates/panopoly/fieldable-panels-pane.tpl.php
@@ -1,4 +1,24 @@
 <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php print render($title_suffix); ?>
-  <?php print render($content); ?>
+  <?php print render($content['field_basic_text_text']); ?>
+  <?php
+
+  if (isset($field_video_file)) {
+    $filemime = $field_video_file['0']['filemime'];
+    if ($filemime == 'video/youtube' || $filemime == 'video/vimeo') {
+      if (isset ($field_video_pane_thumbnail)){
+      print create_video_file_template($field_video_file, $field_video_pane_thumbnail);
+      }
+    }
+  }    
+  ?>
 </div>
+
+<?php if (isset($content['field_caption'])): ?>
+  <div class="caption">
+   <?php print render($content['field_caption']); ?>
+    </div>
+<?php endif; ?>
+
+
+
