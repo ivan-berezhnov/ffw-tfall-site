f47bacf8689541d0b079aa645485c365ac0033a7
Kathy Chavez <kappaluppa2@gmail.com>
2016-01-28 19:56:32 -0800

    replace fieldable-panels-pane.tpl.php

--- docroot/sites/all/themes/tweme/templates/panopoly/fieldable-panels-pane.tpl.php
+++ docroot/sites/all/themes/tweme/templates/panopoly/fieldable-panels-pane.tpl.php
@@ -1,6 +1,7 @@
 <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php print render($title_suffix); ?>
   <?php print render($content['field_basic_text_text']); ?>
+  <?php print render($content['field_basic_image_image']); ?>
   <?php
 
   if (isset($field_video_file)) {
@@ -8,6 +9,9 @@
     if ($filemime == 'video/youtube' || $filemime == 'video/vimeo') {
       if (isset ($field_video_pane_thumbnail)){
       print create_video_file_template($field_video_file, $field_video_pane_thumbnail);
+      }
+      else{
+        print create_video_file_template($field_video_file);
       }
     }
   }    
