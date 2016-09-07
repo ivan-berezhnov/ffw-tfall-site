818649aea8eee46c4c8a1978516c8175a9bdf1cb
Kappaluppa <Kappaluppa2@gmail.com>
2016-09-06 23:21:03 -0700

    impact update

--- docroot/sites/all/themes/tweme/templates/bean/bean--2-tier-graphic-read-more.tpl.php
+++ docroot/sites/all/themes/tweme/templates/bean/bean--2-tier-graphic-read-more.tpl.php
@@ -27,7 +27,7 @@
  * @see template_process()
  */
 ?>
-<div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+<div class="span4 <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <div class="content"<?php print $content_attributes; ?>>
     <?php
